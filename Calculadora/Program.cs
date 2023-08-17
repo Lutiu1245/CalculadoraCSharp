2e2dc60954efabac39e30049dd43a381d7913c45
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-08-17 09:29:33 -0300

    Sub, Div e Mult

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,7 +6,10 @@ namespace Calculadora
     {   
         static void Main(string[] args)
         {
-           Soma();
+        //    Soma();
+        //    Subtracao();
+        // divisao();
+        multiplicacao();
         }
         static void Soma()
         {
@@ -22,5 +25,48 @@ namespace Calculadora
 
             Console.WriteLine($"O resultado da soma é: {valorDoPrimeiroNumero + valorDoSegundoNumero}");
         }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro número:");
+            float valorDoPrimeiroNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            float valorDoSegundoNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da subtração é: {valorDoPrimeiroNumero - valorDoSegundoNumero}");
+        }
+        static void divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro número:");
+            float valorDoPrimeiroNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            float valorDoSegundoNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da divisão é: {valorDoPrimeiroNumero / valorDoSegundoNumero}");
+        }
+        static void multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro número:");
+            float valorDoPrimeiroNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            float valorDoSegundoNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da multiplicação é: {valorDoPrimeiroNumero * valorDoSegundoNumero}");
+        }
     }
+        
 }
