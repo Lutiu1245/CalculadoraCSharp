912fea56d0d436af3f3d3df61a19772af83d6939
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-08-17 09:22:02 -0300

    Feito Soma

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+           Soma();
+        }
+        static void Soma()
+        {
+             Console.Clear();
+
+            Console.WriteLine("Digite o primeiro número:");
+            float valorDoPrimeiroNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            float valorDoSegundoNumero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da soma é: {valorDoPrimeiroNumero + valorDoSegundoNumero}");
+        }
+    }
+}
